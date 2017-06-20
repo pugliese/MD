108ce4eacaed9fafc88117d0a3708b703b9df11b
Facundo Luciano Sanchez <flsanchez94@gmail.com>
2017-06-19 21:52:06 -0300

    [FACU]: La funcion calcular fuerzas pareceria estar funcionando, no se si los valores estan bien, pero calcula las fuerzas

--- src/avanzar.c
+++ src/avanzar.c
@@ -3,3 +3,104 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+
+int Verlet(double *vector_posvel, double *vector_fuerza, int N,
+            double *LUTF, double Ntabla, double m, double h)
+{
+
+  //insert code here
+
+  return 0;
+}
+
+
+//Fij es una matriz antisimetrica de traza nula que guarda la fza que ejerce la
+//particula j sobre la particula i. Proyectando adecuadamente cada termino, la
+//suma sobre una fila me da la fuerza total sobre la particula.
+
+//R variable auxiliar que guarda la distancia entre dos particulas i y j.
+
+//F variable auxiliar que guarda la fuerza entre dos particulas i y j.
+
+int Calcular_Fuerzas(double *vector_posvel, double *vector_fuerza, int N, double *LUTF, int Ntabla){
+
+  double *Fij = malloc(N*N*sizeof(double));
+  double R,F;
+
+  for(int i=0; i<3*N; i++){
+    vector_fuerza[i] = 0;
+  }
+
+  for(int i=0; i<N; i++){
+    for(int j=i+1; j<N; j++){
+      R = Distancia(vector_posvel,N,i,j);
+      F = Valor_LUT(LUTF,Ntabla,R);
+      Fij[i*N+j] = F/R;
+      Fij[j*N+i] = -Fij[i*N+j];
+    }
+  }
+
+  //el primer termino acumula, el segundo es la proyeccion
+
+  for(int i=0; i<N; i++){
+    for(int j=0; j<3; j++){
+      for(int k=0; k<N; k++){
+        vector_fuerza[i+j*N] = vector_fuerza[i+j*N]+(vector_posvel[k+j*N]-vector_posvel[i+j*N])*Fij[i*N+k];
+      }
+    }
+  }
+
+  free(Fij);
+  return 0;
+}
+
+double Distancia(double *pos, int N, int i, int j){
+
+  double dist = 0;
+
+  for(int k=0; k<3; k++){
+    dist = dist + (pos[i+k*N]-pos[j+k*N])*(pos[i+k*N]-pos[j+k*N]);
+  }
+
+  return sqrt(dist);
+
+}
+
+//step es el paso que se uso para la LUT
+
+//idx es el indice que le corresponde al valor mas cercano (por debajo)
+//en la LUT al R usado
+
+double Valor_LUT(double *LUT, int Ntabla, double R){
+
+  double step = 3.0/Ntabla;
+  int idx = floor(R/step);
+  double res = 0;
+
+  if(R>3) res = 0; //si es >3 devuelve 0, no hay interaccion pues no se ven;
+  else if(R<step) res = LUT[0]; //sino, si es menor que step,
+                                //devuelve la minima posicion;
+  else if(R/step == idx) res = LUT[idx]; // sino, si justo R es un valor
+                                         //perteneciente a alguna posicion
+                                         //de la LUT, devuelve ese valor;
+  else res = Interpol(LUT,R,step); //y sino, interpola linealmente.
+
+  return res;
+
+}
+
+//m y b son la pendiente y ordenada del segmento que une los dos puntos cuyo
+//intervalo contiene a R.
+
+//No me pude resistir con el nombre de la funcion.
+
+double Interpol(double *LUT, double R, double step){
+
+  int idx = floor(R/step);
+
+  double m = (LUT[idx+1]-LUT[idx])/step;
+  double b = LUT[idx]-m*idx*step;
+
+  return b + m * R;
+
+}
--- src/avanzar.h
+++ src/avanzar.h
@@ -1,4 +1,11 @@
 #ifndef AVANZAR_H
 #define AVANZAR_H
 
+int Verlet(double *vector_posvel, double *vector_fuerza, int N,
+            double *LUTF, double Ntabla, double m, double h);
+int Calcular_Fuerzas(double *vector_posvel, double *vector_fuerza, int N, double *LUTF, int Ntabla);
+double Distancia(double *pos, int N, int i, int j);
+double Valor_LUT(double *LUT, int Ntable, double R);
+double Interpol(double *LUT, double R, double step);
+
 #endif
