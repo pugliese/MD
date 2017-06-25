08ced4b05f7bf23b8053a34322c7d49452d864bf
Facundo Luciano Sanchez <flsanchez94@gmail.com>
2017-06-25 10:33:39 -0300

    Se limpio un poco grabar.c

--- src/grabar.c
+++ src/grabar.c
@@ -3,10 +3,10 @@
 #include <string.h>
 #include "grabar.h"
 
-int Grabar_Pos(double *vector_posvel, int N, FILE *fs){
+int Grabar_PDB(double *vector_posvel, int N, double L, FILE *fs){
 
   for(int i=0; i<N; i++){
-    Grabar_Particula(vector_posvel,N,fs,i);
+    Grabar_Particula_PDB(vector_posvel,N,L,fs,i);
   }
   fprintf(fs,"END\n");
 
@@ -14,43 +14,22 @@ int Grabar_Pos(double *vector_posvel, int N, FILE *fs){
 
 }
 
-int Grabar_Particula(double *vector_posvel, int N, FILE *fs, int j){
+int Grabar_Particula_PDB(double *vector_posvel, int N, double L, FILE *fs, int j){
 
-  char atom[6]="ATOM  ";
+  char atom[7]= "ATOM  \0";
   char number[7];
-  char blank[19];
-  char x[9];
-  char y[9];
-  char z[9];
-  char aux[50];
+  char blank[19] = "N                 \0";
+  double s = 5; //s es un factor de escala y tiene que estar entre 0 y 9.9999 para que no se vaya de lugar las ubicaciones
 
   sprintf(number,"%d",j+1);
-  if(strlen(number)<6) for(int i = strlen(number);i<6;i++) number[i] = ' ';
+  if(strlen(number)<6){
+    for(int i = strlen(number);i<6;i++){
+      number[i] = ' ';
+    }
+  }
   number[6] = '\0';
 
-  for(int i=0;i<18;i++) blank[i] = ' ';
-  blank[18] = '\0';
-
-  sprintf(aux,"%f",vector_posvel[j]);
-  if(strlen(aux)<7) for(int i = strlen(aux);i<7;i++) aux[i] = ' ';
-  else for(int i=0;i<7;i++) x[i]=aux[i];
-  x[7] = ' ';
-  x[8] = '\0';
-
-
-  sprintf(aux,"%f",vector_posvel[j+N]);
-  if(strlen(aux)<7) for(int i = strlen(aux);i<7;i++) aux[i] = ' ';
-  else for(int i=0;i<7;i++) y[i]=aux[i];
-  y[7] = ' ';
-  y[8] = '\0';
-
-  sprintf(aux,"%f",vector_posvel[j+2*N]);
-  if(strlen(aux)<7) for(int i = strlen(aux);i<7;i++) aux[i] = 'a';
-  else for(int i=0;i<7;i++) z[i]=aux[i];
-  z[7] = ' ';
-  z[8] = '\0';
-
-  fprintf(fs,"%s%s%s%s%s%s\n",atom,number,blank,x,y,z);
+  fprintf(fs,"%s%s%s%.5f %.5f %.5f\n",atom,number,blank,vector_posvel[j]*s/L,vector_posvel[j+N]*s/L,vector_posvel[j+2*N]*s/L);
 
   return 0;
 
--- src/grabar.h
+++ src/grabar.h
@@ -1,7 +1,7 @@
 #ifndef GRABAR_H
 #define GRABAR_H
 
-int Grabar_Pos(double* vector_posvel, int N, FILE *fs);
-int Grabar_Particula(double *vector_posvel, int N, FILE *fs, int j);
+int Grabar_PDB(double* vector_posvel, int N, double L, FILE *fs);
+int Grabar_Particula_PDB(double *vector_posvel, int N, double L, FILE *fs, int j);
 
 #endif
