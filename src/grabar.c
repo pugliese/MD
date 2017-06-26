9a82470ca4acc131a242bd7499d73300c84a8279
Facundo Luciano Sanchez <flsanchez94@gmail.com>
2017-06-26 20:34:25 -0300

    [FACU] optimizada la funcion de grabar particula (ahora sin for's)

--- src/grabar.c
+++ src/grabar.c
@@ -17,19 +17,10 @@ int Grabar_PDB(double *vector_posvel, int N, double L, FILE *fs){
 int Grabar_Particula_PDB(double *vector_posvel, int N, double L, FILE *fs, int j){
 
   char atom[7]= "ATOM  \0";
-  char number[7];
   char blank[19] = "N                 \0";
   double s = 5; //s es un factor de escala y tiene que estar entre 0 y 9.9999 para que no se vaya de lugar las ubicaciones
 
-  sprintf(number,"%d",j+1);
-  if(strlen(number)<6){
-    for(int i = strlen(number);i<6;i++){
-      number[i] = ' ';
-    }
-  }
-  number[6] = '\0';
-
-  fprintf(fs,"%s%s%s%.5f %.5f %.5f\n",atom,number,blank,vector_posvel[j]*s/L,vector_posvel[j+N]*s/L,vector_posvel[j+2*N]*s/L);
+  fprintf(fs,"%s%06d%s%.5f %.5f %.5f\n",atom,j+1,blank,vector_posvel[j]*s/L,vector_posvel[j+N]*s/L,vector_posvel[j+2*N]*s/L);
 
   return 0;
 
