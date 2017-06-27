04149670352876080f9fb7660ad3632a2386acfc
Facundo Luciano Sanchez <flsanchez94@gmail.com>
2017-06-27 17:04:50 -0300

    [FACU] añadido el header con propiedades de estructura al grabar.c

--- src/grabar.c
+++ src/grabar.c
@@ -5,8 +5,12 @@
 
 int Grabar_PDB(double *vector_posvel, int N, double L, FILE *fs){
 
+  double s = 5; //s es un factor de escala y tiene que estar entre 0 y 9.9999 para que no se vaya de lugar las ubicaciones
+
+  fprintf(fs, "CRYST1%08.4f %08.4f %08.4f %06.2f %06.2f %06.2f\n",s,s,s,90.0,90.0,90.0);
+
   for(int i=0; i<N; i++){
-    Grabar_Particula_PDB(vector_posvel,N,L,fs,i);
+    Grabar_Particula_PDB(vector_posvel,N,L,fs,i,s);
   }
   fprintf(fs,"END\n");
 
@@ -14,11 +18,10 @@ int Grabar_PDB(double *vector_posvel, int N, double L, FILE *fs){
 
 }
 
-int Grabar_Particula_PDB(double *vector_posvel, int N, double L, FILE *fs, int j){
+int Grabar_Particula_PDB(double *vector_posvel, int N, double L, FILE *fs, int j, double s){
 
   char atom[7]= "ATOM  \0";
   char blank[19] = "N                 \0";
-  double s = 5; //s es un factor de escala y tiene que estar entre 0 y 9.9999 para que no se vaya de lugar las ubicaciones
 
   fprintf(fs,"%s%06d%s%.5f %.5f %.5f\n",atom,j+1,blank,vector_posvel[j]*s/L,vector_posvel[j+N]*s/L,vector_posvel[j+2*N]*s/L);
 
--- src/grabar.h
+++ src/grabar.h
@@ -2,6 +2,6 @@
 #define GRABAR_H
 
 int Grabar_PDB(double* vector_posvel, int N, double L, FILE *fs);
-int Grabar_Particula_PDB(double *vector_posvel, int N, double L, FILE *fs, int j);
+int Grabar_Particula_PDB(double *vector_posvel, int N, double L, FILE *fs, int j, double s);
 
 #endif
